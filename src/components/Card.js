129fddc9a5b5dd574d4b4b26de198f2834ccfaef
skydragon <bwalikurunda@gmail.com>
2021-08-04 16:23:35 +0100

    prelim implementation

--- src/components/Card.js
+++ src/components/Card.js
@@ -9,14 +9,19 @@ function Card({imageSource, poolName})  {
             <img className="logo" src={imageSource} alt="logo" height="50" width="50"></img>
 
             <p className="description">{poolName}</p>
-            
+
+            {/* <div className="cardData"> */}
+
             <p>Current Projected Yield (APY) </p>
             
             <p>Current Deposits </p>
             <p>Your Deposits </p>
 
-            {/* <Button color='white' text='Connect now'/> */}
              <Button buttonStyle='btn--outline'>CONNECT</Button>
+
+            
+            
+         
         
         </div>
         
---
+++ src/components/Footer.js
@@ -0,0 +1,113 @@
+
+
+function Footer() {
+    return (
+
+        
+
+<footer id="footer" className="footer">
+        
+
+        <div className="rari-logo2">
+          <img src="./images/rari.png" alt="Rari" height="75" />
+        </div>
+
+        <div className="footer__addr">
+          <h1 className="footer__logo">Rari Capital</h1>
+          
+
+
+         
+        </div>
+
+        <ul className="footer__nav">
+          <li className="nav__item">
+            <h2 className="nav__title">PRODUCTS</h2>
+
+            <ul className="nav__ul">
+              <li>
+                <a href="#">Earn</a>
+              </li>
+
+              <li>
+                <a href="#">Tranches</a>
+              </li>
+
+              <li>
+                <a href="#">Governance</a>
+              </li>
+            </ul>
+          </li>
+
+          <li className="nav__item">
+            <h2 className="nav__title">RESOURCES</h2>
+
+            <ul className="nav__ul">
+              <li>
+                <a href="#">About</a>
+              </li>
+
+              <li>
+                <a href="#">Medium</a>
+              </li>
+
+              <li>
+                <a href="#">FAQ</a>
+              </li>
+
+              <li>
+                <a href="#">DeFi Pulse</a>
+              </li>
+            </ul>
+          </li>
+
+        
+          <li className="nav__item">
+            <h2 className="nav__title">RGT</h2>
+
+            <ul className="nav__ul">
+              <li>
+                <a href="#">Coingecko</a>
+              </li>
+
+              <li>
+                <a href="#">Buy with USD</a>
+              </li>
+              <li>
+                <a href="#">Learn</a>
+              </li>
+            </ul>
+          </li>
+        
+
+          <li className="nav__item">
+            <h2 className="nav__title">COMMUNITIES</h2>
+
+            <ul className="nav__ul">
+              <li>
+                <a href="#">Discord</a>
+              </li>
+              <li>
+                <a href="#">Twitter</a>
+              </li>
+              <li>
+                <a href="#">Youtube</a>
+              </li>
+            </ul>
+          </li>
+        </ul>
+
+        <div className="legal">
+          <p>&copy; 2021 Rari Capital. All rights reserved.</p>
+
+          
+        </div>
+
+        
+      </footer>
+            
+     
+    )
+}
+
+export default Footer
